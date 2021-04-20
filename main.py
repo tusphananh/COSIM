5224567f02652a06f8b6ed9a9133ed6c7fb8c5aa
Tu Phan <tuphan@Tus-MacBook-Pro.local>
2021-04-20 16:46:14 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from PyQt5 import QtWidgets
+import PyQt5.QtWidgets as qtw
+import sys
+
+
+class MainFrame(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("COSIM")
+        self.resize(1000, 500)
+        self.setLayout(qtw.QVBoxLayout())
+        self.executeFrame()
+        self.codeFrame()
+        self.resultFrame()
+
+        self.show()
+
+    def executeFrame(self):
+        executeButtons = qtw.QWidget()
+        executeButtons.setLayout(qtw.QVBoxLayout())
+        runButton = qtw.QPushButton('Run')
+        runButton.setMaximumWidth(100)
+        runButton.setMaximumHeight(50)
+        self.layout().addWidget(runButton)
+
+    def codeFrame(self):
+        codeTextArea = qtw.QWidget()
+        codeTextArea.setLayout(qtw.QVBoxLayout())
+        codeArea = qtw.QPlainTextEdit()
+        self.layout().addWidget(codeArea)
+
+    def resultFrame(self):
+        terminal = qtw.QLabel("Terminal")
+        output = qtw.QLabel("Output")
+        tabwidget = qtw.QTabWidget()
+        tabwidget.addTab(terminal, "Terminal")
+        tabwidget.addTab(output, "Output")
+        self.layout().addWidget(tabwidget)
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication([])
+    mainFrame = MainFrame()
+    app.exec_()
